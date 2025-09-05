edb8e32f381d31da7ed5956a90f4f95db026a3f8
Ukyo-Sugimoto <Ukyo-Sugimoto@example.com>
2025-09-05 22:09:28 +0900

    なまえ入力、なまえ削除を追加

--- encore/Controllers/HomeController.cs
+++ encore/Controllers/HomeController.cs
@@ -13,18 +13,20 @@ namespace WebApp.Controllers
         }
 
         [HttpPost]
-        public IActionResult Insert()
+        public IActionResult Insert(string name)
         {
             try
             {
                 using var conn = new NpgsqlConnection(connString);
                 conn.Open();
 
-                string sql = "INSERT INTO mst_users (id, name, mail, create_date) VALUES (3, 'ukyo', 'u@gmail.com', current_timestamp)";
+                string sql = "insert into mst_users(name, create_date, edit_date) values (@name, date_trunc('second', current_timestamp), date_trunc('second', current_timestamp))";
                 using var cmd = new NpgsqlCommand(sql, conn);
+                cmd.Parameters.AddWithValue("@name", name);
+
                 cmd.ExecuteNonQuery();
 
-                ViewBag.Message = "データを挿入しました！";
+                ViewBag.Message =  name + "　でなまえを登録しました！";
             }
             catch (Exception ex)
             {
@@ -33,5 +35,29 @@ namespace WebApp.Controllers
 
             return View("Index");
         }
+
+        public IActionResult Delete(string delname)
+        {
+            try
+            {
+                using var conn = new NpgsqlConnection(connString);
+                conn.Open();
+
+                string sql = "update mst_users set edit_date = date_trunc('second', current_timestamp), delete_date = date_trunc('second', current_timestamp) where name = @name";
+                using var cmd = new NpgsqlCommand(sql, conn);
+                cmd.Parameters.AddWithValue("@name", delname);
+
+                cmd.ExecuteNonQuery();
+
+                ViewBag.Message = delname + "　のなまえを削除しました。";
+            }
+            catch (Exception ex)
+            {
+                ViewBag.Message = "エラー: " + ex.Message;
+            }
+
+            return View("Index");
+        }
+
     }
 }
